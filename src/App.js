83668ebfbf38f0203f9f7f25479f8752c0c3fef0
Samuel <43027737+samuel929@users.noreply.github.com>
2023-02-20 14:18:33 +0200

    part 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const now=new Date();
+  const a=10;
+  const b=20
+  console.log(now,a+b)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <p>Hello world, it is {now.toString()}</p>
+       <p>{a} plus  {b} is {a+b}</p>
     </div>
   );
 }
